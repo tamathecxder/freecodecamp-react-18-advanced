88e7c8bef7906f31fd2cc904c3628095bfa369e6
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-06-29 21:30:58 +0700

    chore: put starter component (useState basics)

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  const [number, setNumber] = useState(0);
+  
+  const handleCounter = () => {
+    return setNumber(number + 1);
+  }
+  
+  return (
+    <div>
+      <h2>useState basics</h2>
+      <p>Number: <span>{number}</span></p>
+      <button onClick={handleCounter}>Click Me</button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
