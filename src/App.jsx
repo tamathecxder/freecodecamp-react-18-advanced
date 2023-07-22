8fa26b00f7f486df8d0c8b538de34ec85bb27f5d
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-07-22 12:17:35 +0700

    refactor: update main component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import FetchingData from  './tutorial/02-useEffect/starter/04-fetch-data';
+import MutlipleReturnFetch from  './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
 
 function App() {
   return (
     <div className="container">
-      <FetchingData />
+      <MutlipleReturnFetch />
     </div>
   )
 }
